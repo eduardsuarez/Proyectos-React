4da1b85d2a9a31bac869042a11bbd4af799a364f
Eduard Suárez <suareze205@gmail.com>
2023-03-03 12:02:05 -0500

    Extraer archivos para guardar en el loocalStorage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,27 +1,33 @@
 import { useState } from "react";
-import confetti from "canvas-confetti"
+import confetti from "canvas-confetti";
 import { Square } from "./components/Square";
-import { TURNS} from "./constants";
+import { TURNS } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
 
-
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    if (boardFromStorage) return JSON.parse(boardFromStorage);
+    return Array(9).fill(null);
+  });
 
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
+
   // null es que no hay ganador, false es que hay empate
   const [winner, setWinner] = useState(null);
-
-  
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
-  };
 
-  
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
+  };
 
   const updateBoard = (index) => {
     // no actualizamos esta posición si ya tiene algo
@@ -34,11 +40,13 @@ function App() {
     //Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
-
+    //guardar aquí partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
     //revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
-      confetti()
+      confetti();
       setWinner(newWinner);
     } else if (checkEndGame(newBoard)) {
       setWinner(false);
@@ -66,7 +74,6 @@ function App() {
       </section>
 
       <WinnerModal resetGame={resetGame} winner={winner} />
-      
     </main>
   );
 }
