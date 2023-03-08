c499e16b0b938ea01a153572f0c0a5343f3d3d7b
Eduard Suárez <suareze205@gmail.com>
2023-03-08 10:58:20 -0500

    Init project 06

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
-function App() {
-
-
+function App () {
   return (
-    
+    <h1>Shopping Cart</h1>
   )
 }
 
