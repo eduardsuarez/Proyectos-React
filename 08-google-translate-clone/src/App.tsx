0e71960be7d200d4a7335d31712ecebb3bb05b17
Eduard Suárez <suareze205@gmail.com>
2023-04-05 10:53:48 -0500

    Se ordenan carpetas de proyectos anteriores y se agrega proyecto google-translate-clone

---
+++ 08-google-translate-clone/src/App.tsx
@@ -0,0 +1,34 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import './App.css'
+import { useStore } from './hooks/useStore'
+import { AUTO_LANGUAGE } from './constants'
+import { ArrowsIcon } from './components/Icons'
+
+function App () {
+  const { fromLanguage, toLanguage, interchangeLanguages } = useStore()
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+
+        <Col>
+          <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
+            <ArrowsIcon/>
+            </Button>
+        </Col>
+
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+
+    </Container>
+  )
+}
+
+export default App
---
+++ 08-google-translate-clone/src/constants.ts
@@ -0,0 +1,7 @@
+export const SUPPPORTED_LANGUAGES = {
+  en: 'English',
+  es: 'Español',
+  de: 'Deutsch'
+}
+
+export const AUTO_LANGUAGE = 'auto'
