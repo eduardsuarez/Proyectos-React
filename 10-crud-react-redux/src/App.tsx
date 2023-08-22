d15ebe57db37ee63dc2aa53b360c1de52a310710
Eduard Suárez <suareze205@gmail.com>
2023-08-22 12:00:16 -0500

    CreateNewUser component, action to create a new user.

--- 10-crud-react-redux/src/App.tsx
+++ 10-crud-react-redux/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { CreateNewUser } from "./components/CreateNewUsers";
 import { ListOfUsers } from "./components/ListOfUsers";
 
 function App() {
 	return (
 		<>
 			<ListOfUsers />
+			<CreateNewUser />
 		</>
 	);
 }
---
+++ 10-crud-react-redux/src/components/CreateNewUsers.tsx
@@ -0,0 +1,53 @@
+import { Badge, Button, Card, TextInput, Title } from "@tremor/react";
+import { useState } from "react";
+import { useUserActions } from "../hooks/useUserActions";
+
+export function CreateNewUser() {
+	const { addUser } = useUserActions();
+	const [result, setResult] = useState<"ok" | "ko" | null>(null);
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormEvent>) => {
+		event.preventDefault();
+		setResult(null);
+
+		const form = event.target;
+
+		const formData = new FormData(form);
+
+		const name = formData.get("name") as string;
+		const email = formData.get("email") as string;
+		const github = formData.get("github") as string;
+
+		if (!name || !email || !github) {
+			return setResult("ko");
+		}
+
+		addUser({ name, email, github });
+		setResult("ok");
+		form.reset();
+	};
+
+	return (
+		<Card style={{ marginTop: "16px" }}>
+			<Title>Create new User</Title>
+
+			<form onSubmit={handleSubmit} className="">
+				<TextInput name="name" placeholder="Aquí el nombre" />
+				<TextInput name="email" placeholder="Aquí el email" />
+				<TextInput name="github" placeholder="Aquí el usuario de Github" />
+
+				<div>
+					<Button type="submit" style={{ marginTop: "16px" }}>
+						Crear Usuario
+					</Button>
+					<span>
+						{result === "ok" && (
+							<Badge color="green">Guardado correctamente</Badge>
+						)}
+						{result === "ko" && <Badge color="red">Error en los campos</Badge>}
+					</span>
+				</div>
+			</form>
+		</Card>
+	);
+}
